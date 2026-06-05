4e688ef09adaba426fd358c00667e47d42f26c9e
rescobar <ricardo_escobar@outlook.com>
2026-06-05 17:57:02 -0600

    [UPDATE] - Se rediseño la pantalla principal

--- core/ui/src/main/java/com/famessa/deli_antojito/core/ui/theme/Color.kt
+++ core/ui/src/main/java/com/famessa/deli_antojito/core/ui/theme/Color.kt
@@ -1,7 +1,7 @@
 package com.famessa.deli_antojito.core.ui.theme
 
 import androidx.compose.ui.graphics.Color
-
+/*
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -16,11 +16,16 @@ val CyanogenGray = Color(0xFF344054)
 val CyanogenDarkBlue = Color(0xFF34669C)
 val CyanogenBlue = Color(0xFF338BE3)
 val CyanogenLightBlue = Color(0xFF75b0EC)
+*/
 
-
-// Famessa Color Palette 1
-var AquaLv1 = Color(0xFFa9e0d4)
-var AquaLv2 = Color(0xFF5fc8b0)
-var AquaLv3 = Color(0xFF3cafa9)
-var AquaLv4 = Color(0xFF2e8e8e)
-var AquaLv5 = Color(0xFF1a6b69)
+// AquaVida mockup palette
+val AquaMist = Color(0xFFEAFBF8)
+val AquaSurface = Color(0xFFD5F5EF)
+val AquaChip = Color(0xFFB7ECE4)
+val AquaAccent = Color(0xFF64D4C7)
+val AquaLv1 = Color(0xFFA9E0D4)
+val AquaLv2 = Color(0xFF5FC8B0)
+val AquaLv3 = Color(0xFF3CAFA9)
+val AquaLv4 = Color(0xFF0B7779)
+val AquaLv5 = Color(0xFF04595D)
+val AquaDeep = Color(0xFF003F45)
